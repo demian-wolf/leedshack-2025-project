a4fad8b78472b74776edea25d24860ab13a7d490
Jiahe321 <sc23j4l@leeds.ac.uk>
2025-02-08 20:14:44 +0000

    Changes to be committed:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import styles from './App.module.css';
-import {Route, Routes} from "react-router-dom";
-import {Signup} from "./Pages/Signup/Signup";
-
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Signup } from "./Pages/Signup/Signup";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainContent from "./components/MainContent/MainContent";
+import Footer from "./components/Footer/Footer";
+import './index.css';
 
 const App = () => {
+  const location = useLocation();   
+
+  const isSignupPage = location.pathname === "/signup";
+
   return (
     <div className={styles.app}>
+      {!isSignupPage && <Header />}
 
-        <Routes>
-          <Route path="/" element={<Signup/>} />
-        </Routes>
+      <div className={styles.content}>
+        {!isSignupPage && <Sidebar />}
 
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<MainContent />} />
+          </Routes>
+        </main>
+      </div>
+
+      {!isSignupPage && <Footer />}
     </div>
   );
-}
+};
 
 export default App;
-
