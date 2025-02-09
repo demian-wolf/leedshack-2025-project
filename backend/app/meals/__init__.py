579ed7761e913d2d2bc64398bc4fb3be4e3ce12c
Demian Volkov <demianwolfssd@gmail.com>
2025-02-09 08:00:34 +0000

    Add url rule for LetsCookMealView

--- backend/app/meals/__init__.py
+++ backend/app/meals/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .views import GenerateMealView
+from .views import GenerateMealView, LetsCookMealView
 
 
 meals_bp = Blueprint("meals", __name__)
@@ -8,5 +8,11 @@ meals_bp = Blueprint("meals", __name__)
 meals_bp.add_url_rule(
     "/meals/generate",
     view_func=GenerateMealView.as_view("generate_meal_view"),
+    methods=["GET"],
+)
+
+meals_bp.add_url_rule(
+    "/meals/grow",
+    view_func=LetsCookMealView.as_view("letscook_meal_view"),
     methods=["GET"],
 )
