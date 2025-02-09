88c51955e635ffe3db96ad1d06915fd6ffddf22e
Demian Volkov <demianwolfssd@gmail.com>
2025-02-09 08:59:13 +0000

    Rename Home -> Dashboard & Remove Contact

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -10,9 +10,8 @@ const Header = () => {
         <h1>Mealio</h1>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/">Dashboard</a></li>
+            <li><a href="/about">About us</a></li>
           </ul>
         </nav>
       </div>
